224770292c948b31619c64ac085d09cb7326a9ef
Artem Trybrat <Artem_Trybrat@epam.com>
2018-08-15 12:15:29 +0300

    Added profile.d configuration for jdk8

--- java/profile.d-java-jdk8.sh
+++ java/profile.d-java-jdk8.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cat <<EOT >> /etc/profile.d/java.sh
+cat <<EOT > /etc/profile.d/java.sh
 export JAVA_HOME="/opt/java"
-export JDK_HOME="$JAVA_HOME"
+export JDK_HOME="\$JAVA_HOME"
 
 export JAVA_OPTS="-server -Xms256m -Xmx4096m -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=512m"
 
-export PATH="$PATH:$JAVA_HOME/bin"
+export PATH="\$PATH:\$JAVA_HOME/bin"
 
 EOT
